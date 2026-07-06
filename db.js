ca8557c842b0d9e784a166fb4783280af600978e
Anish <anishchatla06@gmail.com>
2026-07-06 15:43:23 +0530

    MongoDB shifting to online server

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-require('.env').config();
+require('dotenv').config();
 
 //const mongoURL = process.env.MONGODB_URL_LOCAL;
 const mongoURL = process.env.MONGODB_URL; 
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const db= require('./db');
-require('.env').config();
+require('dotenv').config();
 
 const bodyParser=require('body-parser');   //body parser
 app.use(bodyParser.json());          //everything will be saved in req.body
